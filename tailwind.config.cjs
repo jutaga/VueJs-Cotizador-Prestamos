3fcfd9bd52f4286b278529ee177a91c4827b415b
jutaga <juancho9615@hotmail.com>
2022-11-15 19:00:17 -0500

    Cotizador Vue.Js

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,vue,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
